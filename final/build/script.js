e10da2eea1f47b4791533587fa883400a2db911b
alice-poole <ap3789@drexel.edu>
2022-12-01 08:31:20 -0500

    Hotfix

--- final/build/script.js
+++ final/build/script.js
@@ -1,14 +1,27 @@
 const options = document.querySelectorAll('.sticky');
+const page = document.querySelector('body');
+const pink = document.querySelector('.pickPink');
+const gold = document.querySelector('.pickGold');
+const blue = document.querySelector('.pickBlue');
+var color = 'pink';
 
 options.forEach(button => {
   button.addEventListener('click', () => {
     options.forEach(button => button.classList.remove('pressed'));
     button.classList.add('pressed');
+    color = button.id;
+    choose(color);
     });
 });
 
-const button = document.querySelector('.sticky');
+function click() {
+    
+}
 
-button.addEventListener('click', () => {
-  button.classList.toggle('pressed');
-});
+function choose(color) {
+    document.body.classList.remove('pink','gold', 'blue');
+    document.body.classList.add(color);
+}
+/*document.querySelector(".example");
+document.getElementById("myHeader").innerHTML = "Have a nice day!";
+document.getElementById("pink").classList.toggle('pressed');*/
